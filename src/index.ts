14093dd42ee4b9b49f229ab73746d043d32c6614
Kunho Kim <kh.kim@careerday.jobs>
2022-09-23 18:05:54 +0900

    No default export but just export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import PaginatedCommander from './components/paginatedCommander';
 import usePaginated from './hooks/usePaginated';
 import PaginatedParam from './types/paginatedParam';
 
-export default { PaginatedCommander, usePaginated, PaginatedParam };
+export { PaginatedCommander, usePaginated, PaginatedParam };
